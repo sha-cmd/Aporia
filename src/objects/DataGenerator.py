524e836a3128d9a42a3009417cc385d5400bdef8
romain Boyrie <romain@boyrie.email>
2022-02-04 10:18:32 +0100

    Ajout d’un paramètre pour la compatibilité avec le Hyper Model

--- src/objects/DataGenerator.py
+++ src/objects/DataGenerator.py
@@ -3,12 +3,13 @@ import sys
 from .ModelVariables import ModelVariables
 from tools import data_original_version
 from tools import data_augmented
-
+from tools import BATCH_SIZE
 
 class DataGenerator(ModelVariables):
-    def __init__(self):
+    def __init__(self, batch_size=BATCH_SIZE):
         super().__init__()
         print('Eager loading ready')
+        self.size_batch = batch_size
 
     def __call__(self, path_images, path_masks, data_mix='NA'):
         # Security purpose
@@ -29,9 +30,9 @@ class DataGenerator(ModelVariables):
 
     def data_vo(self):
         print('data_vo')
-        return data_original_version(self.path_images, self.path_masks)
+        return data_original_version(self.path_images, self.path_masks, self.size_batch)
 
     def data_aug(self):
         print('data_aug')
-        return data_augmented(self.path_images, self.path_masks)
+        return data_augmented(self.path_images, self.path_masks, self.size_batch)
 
